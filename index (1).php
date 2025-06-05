587e06e45e93be92da55ab00036155ce184f7aa6
Amar Redžić <amar.redzic@scv.si>
2025-06-05 13:45:45 +0200

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+
+<h1>Dobrodošli v spletni aplikaciji Odbojkarska liga</h1>
+<p>
+    Spletišče omogoča ogled igralcev, ocenjevanje in komentiranje. Prijavi se ali registriraj za sodelovanje.<br>
+
+   <h2> To je šolski projekt zato ne pričakujte preveč.</h2>
+</p>
+
+<img src="img/volleyball.jpg" alt="Odbojka" style="max-width:100%; height:auto; border-radius:10px; margin-top:15px;">
+
+<?php include 'footer.php'; ?>
